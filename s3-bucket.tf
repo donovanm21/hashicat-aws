012946f210aaba9060c314898196eb1095e0393d
Donovan Muskett-Yetts <donovan@appleyeti.co.za>
2023-02-22 03:26:14 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,10 +2,8 @@ module "s3_bucket" {
   source  = "app.terraform.io/TheDon/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket_prefix = "Donovan"
-  bucket = "s3-bucket"
-  acl    = "private"
-
+  bucket_prefix = "donovan"
+  
   versioning = {
     enabled = true
   }
